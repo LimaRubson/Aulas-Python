8236fc68ca3d6e2f5fed297e6011b8647d72d083
System Administrator <root@iMac-de-Rubson.local>
2018-06-15 10:07:58 -0300

    Implementado o conceito de atributo complexo.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,22 @@
 class Pessoa:
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+
+    rubson = Pessoa(nome='Rubson')
+    raira = Pessoa(rubson, nome='Raira')
+    print(Pessoa.cumprimentar(rubson))
+    print(id(rubson))
+    print(rubson.cumprimentar())
+    print(rubson.nome)
+    print(rubson.idade)
+    for filho in raira.filhos:
+        print(filho.nome)
+
