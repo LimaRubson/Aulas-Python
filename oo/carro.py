717827fa746b5f5f41cb79c16822c52b329a9c4b
System Administrator <root@iMac-de-Rubson.local>
2018-06-18 10:01:32 -0300

    Implementado as classes Motor e Direcao.

--- oo/carro.py
+++ oo/carro.py
@@ -39,7 +39,7 @@ O       L
         >>> motor.velocidade
         0
         >>> # Testando Direção
-        >>> direcao = direcao()
+        >>> direcao = Direcao()
         >>> direcao.valor
         'Norte'
         >>> direcao.girar_a_direita()
@@ -91,3 +91,36 @@ O       L
         >>> 'Oeste'
 
 """
+
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
+class Direcao:
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL:OESTE, OESTE: NORTE
+    }
+    rotacao_a_esquerda_doct = {
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+
+class Motor:
+    def __init__(self):
+        self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
