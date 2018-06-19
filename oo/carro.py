3b321816fbb7dbd71e7bd1f992b688982f2027d8
System Administrator <root@iMac-de-Rubson.local>
2018-06-19 10:08:35 -0300

    Implementação da Classe Carro.

--- oo/carro.py
+++ oo/carro.py
@@ -79,18 +79,41 @@ O       L
         >>> carro.calcular_velocidade()
         0
         >>> carro.calcular_direcao()
-        >>> 'Norte'
+        'Norte'
         >>> carro.calcular_direcao()
-        >>> carro.girar_a_direcao()
-        >>> 'Leste'
-        >>> carro.calcular_direcao()
-        >>> carro.girar_a_esquerda()
-        >>> 'Norte'
+        >>> carro.girar_a_direita()
+        'Leste'
         >>> carro.calcular_direcao()
         >>> carro.girar_a_esquerda()
-        >>> 'Oeste'
+        'Norte'
+        >>> carro.calcular_direcao()
+        >>> carro.girar_a_esquerda()
+        'Oeste'
 
 """
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
 
 NORTE = 'Norte'
 SUL = 'Sul'
